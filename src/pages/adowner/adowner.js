a96c64e3b62ac72df1d439057e4ddbcd1363645e
WuChungYueh <ChungYueh@WuChungYuehdeMacBook-Pro.local>
2017-10-30 23:19:30 +0800

    20171029

--- src/pages/adowner/adowner.js
+++ src/pages/adowner/adowner.js
@@ -1,5 +1,87 @@
 // Validating Empty Field
 let mediaSeq = 0;
+window.onload = () =>{
+	// requestADList();
+};
+function requestADList() {
+	let sendObj = {};
+	sendObj.userName = document.getElementById('userName').value;
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', '');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(sendObj));
+	xhr.onload = () => {
+		prodADList(JSON.parse(xhr.responseText));
+	}
+}
+function prodADList(adObj) {
+	let table = document.getElementById('mediaTable');
+	let newTableRow = table.insertRow(-1);
+	let cellData = [];
+	for (i = 0; i < 8; i++) {
+		cellData[i] = newTableRow.insertCell(i);
+	}
+
+	cellData[0].innerHTML = adObj.seqNo.toString();
+
+	// let locationListDiv = document.createElement('div');
+	// locationListDiv.classList.add('locationListBlock');
+	// let locationBox1 = document.createElement('input');
+	// locationBox1.type = 'checkbox';
+	// let locationBox2 = document.createElement('input');
+	// locationBox2.type = 'checkbox';
+	// let locationBox3 = document.createElement('input');
+	// locationBox3.type = 'checkbox';
+	// locationBox1.name = 'media1';
+	// locationBox2.name = 'media2';
+	// locationBox3.name = 'media3';
+	// locationBox1.value = 'Jacky';
+	// locationBox2.value = 'Zenic';
+	// locationBox3.value = 'Jimmy';
+	// let locateBoxBtn = document.createElement('span');
+	// locateBoxBtn.classList.add('locateBtn');
+	// locateBoxBtn.addEventListener('click', confirmLocate);
+	// locateBoxBtn.innerHTML = '確認';
+	// let label1 = document.createElement('label');
+	// let label2 = document.createElement('label');
+	// let label3 = document.createElement('label');
+	// label1.setAttribute('for', 'media1');
+	// label2.setAttribute('for', 'media2');
+	// label3.setAttribute('for', 'media3');
+	// label1.innerHTML = 'Jacky';
+	// label2.innerHTML = 'Zenic';
+	// label3.innerHTML = 'Jimmy';
+
+	
+	// locationListDiv.appendChild(locationBox1);
+	// locationListDiv.appendChild(label1);
+	// locationListDiv.appendChild(locationBox2);
+	// locationListDiv.appendChild(label2);
+	// locationListDiv.appendChild(locationBox3);
+	// locationListDiv.appendChild(label3);
+	// locationListDiv.appendChild(locateBoxBtn);
+	// cellData[4].appendChild(locationListDiv);
+
+	let locate = document.createElement('span');
+	locate.classList.add('locateBtn');
+	locate.addEventListener('click', chooseLocate);
+	locate.innerHTML = '選擇看板';
+	cellData[4].appendChild(locate);
+
+	let videoIcon = document.createElement('i');
+	videoIcon.classList.add('fa', 'fa-film');
+	videoIcon.setAttribute('videoSrc', adPath);
+	videoIcon.addEventListener('click', showVideoFrame);
+	cellData[6].appendChild(videoIcon);
+
+	let removeIcon = document.createElement('i');
+	removeIcon.classList.add('fa', 'fa-trash');
+	removeIcon.addEventListener('click', removeAD);
+	cellData[7].appendChild(removeIcon);
+
+	// mediaSeq++;
+
+}
 function checkFieldEmpty() {
 	let pic = document.getElementById('uploadPic').value;
 	let video = document.getElementById('uploadVideo').value;
@@ -15,11 +97,29 @@ function checkFieldEmpty() {
 //Function To Display Popup
 function showForm() {
 	document.getElementById('popupContainer').style.display = 'flex';
+	document.getElementById('typeDiv').style.display = 'flex';
 	clearFormContent();
 }
 //Function to Hide Popup
 function hideForm() {
 	document.getElementById('popupContainer').style.display = 'none';
+	document.getElementById('picForm').style.display = 'none';
+	document.getElementById('videoForm').style.display = 'none';
+}
+function showPicForm() {
+	document.getElementById('typeDiv').style.display = 'none';
+	document.getElementById('picForm').style.display = 'flex';
+}
+function showVideoForm() {
+	document.getElementById('typeDiv').style.display = 'none';
+	document.getElementById('videoForm').style.display = 'flex';
+}
+function showScreenForm() {
+	document.getElementById('chooseScreen').style.display = 'flex';
+	document.getElementById('screenForm').style.display = 'flex';
+}
+function hideScreenForm() {
+	document.getElementById('chooseScreen').style.display = 'none';
 }
 
 function addAD(pic, video) {
@@ -45,49 +145,50 @@ function addAD(pic, video) {
 		adPath = video;
 	}
 
-	let locationListDiv = document.createElement('div');
-	locationListDiv.classList.add('locationListBlock');
-	// let checkBox = document.createElement('input');
-	// checkBox.type = 'checkbox';
-	let locationBox1 = document.createElement('input');
-	locationBox1.type = 'checkbox';
-	let locationBox2 = document.createElement('input');
-	locationBox2.type = 'checkbox';
-	let locationBox3 = document.createElement('input');
-	locationBox3.type = 'checkbox';
-	locationBox1.name = 'media1';
-	locationBox2.name = 'media2';
-	locationBox3.name = 'media3';
-	locationBox1.value = 'Jacky';
-	locationBox2.value = 'Zenic';
-	locationBox3.value = 'Jimmy';
-	let locateBoxBtn = document.createElement('span');
-	locateBoxBtn.classList.add('locateBtn');
-	locateBoxBtn.addEventListener('click', confirmLocate);
-	locateBoxBtn.innerHTML = '確認';
-	let label1 = document.createElement('label');
-	let label2 = document.createElement('label');
-	let label3 = document.createElement('label');
-	label1.setAttribute('for', 'media1');
-	label2.setAttribute('for', 'media2');
-	label3.setAttribute('for', 'media3');
-	label1.innerHTML = 'Jacky';
-	label2.innerHTML = 'Zenic';
-	label3.innerHTML = 'Jimmy';
+	// let locationListDiv = document.createElement('div');
+	// locationListDiv.classList.add('locationListBlock');
+	// // let checkBox = document.createElement('input');
+	// // checkBox.type = 'checkbox';
+	// let locationBox1 = document.createElement('input');
+	// locationBox1.type = 'checkbox';
+	// let locationBox2 = document.createElement('input');
+	// locationBox2.type = 'checkbox';
+	// let locationBox3 = document.createElement('input');
+	// locationBox3.type = 'checkbox';
+	// locationBox1.name = 'media1';
+	// locationBox2.name = 'media2';
+	// locationBox3.name = 'media3';
+	// locationBox1.value = 'Jacky';
+	// locationBox2.value = 'Zenic';
+	// locationBox3.value = 'Jimmy';
+	// let locateBoxBtn = document.createElement('span');
+	// locateBoxBtn.classList.add('locateBtn');
+	// locateBoxBtn.addEventListener('click', confirmLocate);
+	// locateBoxBtn.innerHTML = '確認';
+	// let label1 = document.createElement('label');
+	// let label2 = document.createElement('label');
+	// let label3 = document.createElement('label');
+	// label1.setAttribute('for', 'media1');
+	// label2.setAttribute('for', 'media2');
+	// label3.setAttribute('for', 'media3');
+	// label1.innerHTML = 'Jacky';
+	// label2.innerHTML = 'Zenic';
+	// label3.innerHTML = 'Jimmy';
 
 	
-	locationListDiv.appendChild(locationBox1);
-	locationListDiv.appendChild(label1);
-	locationListDiv.appendChild(locationBox2);
-	locationListDiv.appendChild(label2);
-	locationListDiv.appendChild(locationBox3);
-	locationListDiv.appendChild(label3);
-	locationListDiv.appendChild(locateBoxBtn);
-	cellData[4].appendChild(locationListDiv);
+	// locationListDiv.appendChild(locationBox1);
+	// locationListDiv.appendChild(label1);
+	// locationListDiv.appendChild(locationBox2);
+	// locationListDiv.appendChild(label2);
+	// locationListDiv.appendChild(locationBox3);
+	// locationListDiv.appendChild(label3);
+	// locationListDiv.appendChild(locateBoxBtn);
+	// cellData[4].appendChild(locationListDiv);
 
 	let locate = document.createElement('span');
 	locate.classList.add('locateBtn');
-	locate.addEventListener('click', chooseLocate);
+	// locate.addEventListener('click', chooseLocate);
+	locate.addEventListener('click', showScreenForm)
 	locate.innerHTML = '選擇看板';
 	cellData[4].appendChild(locate);
 
